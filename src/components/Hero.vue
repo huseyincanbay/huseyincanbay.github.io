6cc8be172b1b755faf555eb1baf5039635261427
Huseyin Canbay <huseyincanbay10@gmail.com>
2023-08-05 18:05:18 +0300

    updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -51,9 +51,9 @@
           <div class="img mt-8 flex flex-wrap justify-center gap-4">
             <a
               class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl hover:from-purple-500 hover:to-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2"
-              href="assets/huseyin-canbay-cv.pdf"
-              type="application/pdf"
+              href="src/assets/huseyin-canbay-cv.pdf"
               target="_blank"
+              type="application/pdf"
             >
               <button type="button">Download CV</button>
             </a>
